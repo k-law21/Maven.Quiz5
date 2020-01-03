1bd6beb328ef8d09d6ab0e438669019a9b9db704
k-law21 <kwame@udel.edu>
2020-01-03 10:17:03 -0500

    Palindrome Eval Complete

--- src/main/java/rocks/zipcode/io/quiz4/fundamentals/PalindromeEvaluator.java
+++ src/main/java/rocks/zipcode/io/quiz4/fundamentals/PalindromeEvaluator.java
@@ -1,13 +1,26 @@
 package rocks.zipcode.io.quiz4.fundamentals;
 
+import java.util.ArrayList;
+import java.util.HashSet;
+import java.util.List;
+import java.util.Set;
+
 /**
  * @author leon on 18/12/2018.
  */
 public class PalindromeEvaluator {
     public static String[] getAllPalindromes(String string) {
 
-        return null;
-    }
+            Set<String> palindromes = new HashSet<>();
+            for (int i = 0; i < string.length(); i++) {
+                for (int j = i + 1; j <= string.length(); j++) {
+                    if (isPalindrome(string.substring(i, j))) {
+                        palindromes.add(string.substring(i, j));
+                    }
+                }
+            }
+            return palindromes.toArray(new String[0]);
+        }
 
     public static Boolean isPalindrome(String string) {
 
